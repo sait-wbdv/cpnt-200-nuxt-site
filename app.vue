8250fb39f08ca4db20c490af2a59f2f6d60f3de6
Ashlyn Knox <ashlyn@lilyx.io>
2024-03-25 11:38:30 -0600

    login and logout added

--- app.vue
+++ app.vue
@@ -1,3 +1,23 @@
+<script setup>
+async function logout() {
+  try {
+    let { error } = await supabase.auth.signOut();
+    if (error) throw error;
+  } catch (error) {
+    console.error(`signout error: ${error}`);
+  }
+}
+</script>
 <template>
+  <header>
+    <div><NuxtLink to="/">Logo</NuxtLink></div>
+    <nav>
+      <NuxtLink to="/login">Login Page</NuxtLink>
+      <NuxtLink to="/signup">Sign Up Page</NuxtLink>
+    </nav>
+    <div>
+      <button @click="logout">logout</button>
+    </div>
+  </header>
   <NuxtPage />
 </template>
--- pages/signup.vue
+++ pages/signup.vue
@@ -2,17 +2,20 @@
 const supabase = useSupabaseClient();
 const email = ref("");
 const password = ref("");
-async function login() {
-  let { data, error } = await supabase.auth.signInWithPassword({
+async function signUp() {
+  const { data, error } = await supabase.auth.signUp({
     email: email.value,
     password: password.value,
+    options: {
+      emailRedirectTo: "http://localhost:3000/confirm",
+    },
   });
 }
 </script>
 <template>
   <main>
     <header>
-      <h1>Login Page</h1>
+      <h1>Sign Up Page</h1>
     </header>
     <form>
       <!-- Bind the varaibles to the input with v-model -->
@@ -24,7 +27,7 @@ async function login() {
         Password:
         <input type="password" name="password" v-model="password" />
       </label>
-      <button @click="login">Login</button>
+      <button @click="signUp">Sign Up</button>
     </form>
   </main>
 </template>
