48634d4214f10ed38bff682c93b97e66f7835a5e
Ashlyn Knox <ashlyn@lilyx.io>
2024-04-02 16:49:39 -0600

    login page minimal example

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,29 @@
+<script setup>
+const email = ref("");
+const password = ref("");
+
+let { data, error } = await supabase.auth.signInWithPassword({
+  email: email.value,
+  password: password.value,
+});
+</script>
 <template>
   <main>
-    <h1>Login</h1>
+    <form>
+      <label for="email">
+        Email:
+        <input type="email" id="email" name="email" v-model="email" />
+      </label>
+      <label for="password">
+        Email:
+        <input
+          type="password"
+          id="password"
+          name="password"
+          v-model="password"
+        />
+      </label>
+      <input type="submit" value="Sign In" @click="useSignIn" />
+    </form>
   </main>
 </template>
