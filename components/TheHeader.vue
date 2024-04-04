6373b878fa88b662093e64c7fba6f9fc01ddaa5a
Ashlyn Knox <ashlyn@lilyx.io>
2024-04-03 18:02:42 -0600

    google added to header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,19 +1,22 @@
 <template>
   <header>
-    <!-- logo -->
-    <div>logo</div>
-    <!-- menu -->
+    <div>Home</div>
     <nav>
       <ul>
         <li><NuxtLink to="/blog">Server Side Blog</NuxtLink></li>
         <li><NuxtLink to="/articles">Client Side Articles</NuxtLink></li>
       </ul>
     </nav>
-    <!-- action -->
     <div>
-      <div>
-        <NuxtLink to="login">Login</NuxtLink>
-      </div>
+      <ul>
+        <li>
+          <NuxtLink to="/signup">Sign up</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/login">Login</NuxtLink>
+        </li>
+      </ul>
+      <button @click="useGoogleLogin">Login with Google</button>
       <button @click="useSignOut">Logout</button>
     </div>
   </header>
